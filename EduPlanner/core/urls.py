9f8a07ecf4bc1191d81bfd7a878b68177fdd859b
Eduardo-Guerra-Lobos <eguerra@usm.cl>
2024-11-22 20:01:30 -0300

    Segundo commit
    Se agrega a settings en installed apps la biblioteca crispy forms
    'crispy_forms',
    'crispy_bootstrap5',
     y bajo de installed apps se agregan estas lineas
    CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
    
    CRISPY_TEMPLATE_PACK = "bootstrap5"
    ademas en las urls de Eduplanner se agrega la ruta de accounts
    path('accounts/', include('django.contrib.auth.urls')),

--- EduPlanner/core/urls.py
+++ EduPlanner/core/urls.py
@@ -3,6 +3,7 @@ from core import views as vistas
 
 urlpatterns = [
     path('', vistas.inicio, name='inicio'),
-    path('segunda/', vistas.segunda, name='segunda'),
+    path('Iniciar Sesion/', vistas.IniciarSesion, name='Iniciar Sesion'), 
+    path('Registrarse/', vistas.Registrarse, name='Registrarse'),   
     
 ]
