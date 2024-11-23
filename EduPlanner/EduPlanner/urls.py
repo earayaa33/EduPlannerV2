bc5387f6668c3c9065c16b8f52c6d637fefde2d6
earayaa33 <earayaa@usm.cl>
2024-11-23 00:12:52 -0300

    Autenticación basica con SimpleJWT

--- EduPlanner/EduPlanner/urls.py
+++ EduPlanner/EduPlanner/urls.py
@@ -28,7 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
     path('api/', include('apicalendario.urls')),
-    
-
     path('accounts/', include('django.contrib.auth.urls')), 
 ]
--- EduPlanner/apicalendario/views.py
+++ EduPlanner/apicalendario/views.py
@@ -1,14 +1,9 @@
 from .models import Evento
-from .permissions import administradorAcademico, usuarioComun
+from rest_framework.permissions import IsAuthenticated
 from .serializers import EventoSerializer
 from rest_framework import viewsets
 
 class EventoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
-
-    def get_permissions(self):
-        if self.action in ['create', 'update', 'destroy']: 
-            return [administradorAcademico()]
-        else:  
-            return [usuarioComun()]
+    permission_classes = (IsAuthenticated,)
