8e38f0397f00f033775bcaa9564ba53bcbf140d0
Eduardo-Guerra-Lobos <eguerra@usm.cl>
2024-11-23 18:22:32 -0300

    Sexto commit
    modificacion del login

--- EduPlanner/core/urls.py
+++ EduPlanner/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from core import views as vistas
 
 urlpatterns = [
-    path('', vistas.inicio, name='inicio'),
+    path('inicio/', vistas.inicio, name='inicio'),
     path('Iniciar Sesion/', vistas.IniciarSesion, name='Iniciar Sesion'), 
     path('Registrarse/', vistas.Registrarse, name='Registrarse'),   
     path('salir/', vistas.salir, name='salir'),
