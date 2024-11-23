c1b19b8ae4d11e91324a9298d32b9974af9c1cca
earayaa33 <earayaa@usm.cl>
2024-11-23 17:02:32 -0300

    Desarollo de la vista de eventos y feriados separada

--- EduPlanner/apicalendario/urls.py
+++ EduPlanner/apicalendario/urls.py
@@ -1,6 +1,7 @@
 from apicalendario import views
 from rest_framework import routers
 from django.urls import path, include
+from .views import EventosYFeriadosAPIView
 
 router = routers.DefaultRouter()
 
@@ -10,9 +11,9 @@ router.register('eventos', views.EventoViewSet, basename='evento')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('eventos-y-feriados/', EventosYFeriadosAPIView.as_view()),
     #path('eventos/', views.EventoListCreateAPIView.as_view()),
     #path('eventos/create', views.EventoCreateAPIView.as_view()),
     #path('eventos/<int:pk>', views.EventoDetalleAPIView.as_view())
     
-
 ]
--- EduPlanner/apicalendario/views.py
+++ EduPlanner/apicalendario/views.py
@@ -1,7 +1,11 @@
 from .models import Evento
+import requests
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser, AllowAny
+from rest_framework.response import Response
 from .serializers import EventoSerializer
 from rest_framework import viewsets, generics
+from rest_framework.views import APIView
+from datetime import date
 
 
 #class EventoListAPIView(generics.ListAPIView):
@@ -28,6 +32,39 @@ class EventoViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
+
+class EventosYFeriadosAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        eventos = Evento.objects.all()
+        eventos_serializados = EventoSerializer(eventos, many=True).data
+
+        url = "https://api.boostr.cl/holidays.json"
+
+        headers = {"accept": "application/json"}
+
+        response = requests.get(url, headers=headers)
+
+        feriados = response.json().get("data",[])
+
+        eventos_a_ordenar = [
+            {"titulo":evento["titulo"], "descripcion":evento["descripcion"], "fecha_inicio":evento["fecha_inicio"], "fechas_finalizacion":evento["fecha_finalizacion"]}
+            for evento in eventos_serializados
+        ]
+        feriados_a_ordenar = [
+            {"titulo":feriado["title"], "fecha_inicio":feriado["date"], "tipo":feriado["type"]}
+            for feriado in feriados
+        ]
+
+        eventos_combinados =  eventos_a_ordenar + feriados_a_ordenar
+
+        for item in eventos_combinados:
+            item["fecha_inicio"] = date.fromisoformat(item["fecha_inicio"])
+
+        eventos_combinados.sort(key=lambda x: x["fecha_inicio"])
+
+        return Response(eventos_combinados)
 
 #class EventoDetalleAPIView(generics.RetrieveUpdateDestroyAPIView):
    # queryset = Evento.objects.all()
