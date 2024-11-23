90b041b12f263c235201bae199c45f9a3b57c88d
earayaa33 <earayaa@usm.cl>
2024-11-23 13:18:36 -0300

    Prohibicion a usuarios no administradores de hacer solicitudes POST

--- EduPlanner/apicalendario/urls.py
+++ EduPlanner/apicalendario/urls.py
@@ -2,14 +2,17 @@ from apicalendario import views
 from rest_framework import routers
 from django.urls import path, include
 
-router = routers.DefaultRouter()
+#router = routers.DefaultRouter()
 
 #Se añaden al router los endpoints a los viewsets
 
-router.register('eventos', views.EventoViewSet)
+#router.register('eventos', views.EventoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    #path('', include(router.urls)),
+    path('eventos/', views.EventoListCreateAPIView.as_view()),
+    #path('eventos/create', views.EventoCreateAPIView.as_view()),
+    path('eventos/<int:pk>', views.EventoDetalleAPIView.as_view())
     
 
 ]
--- EduPlanner/apicalendario/views.py
+++ EduPlanner/apicalendario/views.py
@@ -1,9 +1,32 @@
 from .models import Evento
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser, AllowAny
 from .serializers import EventoSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-class EventoViewSet(viewsets.ModelViewSet):
+
+#class EventoListAPIView(generics.ListAPIView):
+   # queryset = Evento.objects.all()
+    #serializer_class = EventoSerializer
+
+#Contiene todas las opciones
+
+#class EventoViewSet(viewsets.ModelViewSet):
+   #queryset = Evento.objects.all()
+   #serializer_class = EventoSerializer
+
+class EventoListAPIView(generics.ListAPIView):
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
-    permission_classes = (IsAuthenticated,)
+
+class EventoListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Evento.objects.all()
+    serializer_class = EventoSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAdminUser()]
+        return [AllowAny()]
+
+class EventoDetalleAPIView(generics.RetrieveAPIView):
+    queryset = Evento.objects.all()
+    serializer_class = EventoSerializer 
